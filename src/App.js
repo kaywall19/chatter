b1cda4b19b46723814cdc2904c3693110eb68de7
wallek19 <wallek19@uw.edu>
2020-01-17 08:49:16 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <main>
+    <header className= "main-title">
+      <img src="https://img.icons8.com/dotty/80/000000/filled-topic.png" id="logo"/>      
+      Chatter
+      <img src="https://img.icons8.com/android/24/000000/phone.png" id="call"/>
+      <img src="https://img.icons8.com/android/24/000000/search.png" id="search"/>
+      <img src="https://img.icons8.com/ios-glyphs/30/000000/menu-2.png" id="more"/>
+    </header> 
+
+    <TextInput onSend={t=>console.log(t)}/> 
+    <img src="https://img.icons8.com/dotty/80/000000/filled-topic.png" id="background"/>
+    <div class="arrow" id="oneA"></div>
+    <p class="message" id="one">Helloooo</p>
+    <div class="arrow" id="twoA"></div>
+    <p class="message" id="two">Is anyone there?</p>
+    <div class="arrow" id="threeA"></div>
+    <p class="message" id="three">Pls answer</p>
+
+  </main>
+}
+
+function TextInput(props) {
+  const [text, setText] = useState("   ")
+
+  return <div className= "text-input">
+    <img src= "https://img.icons8.com/material-outlined/24/000000/camera--v1.png"/>
+    <img src= "https://img.icons8.com/ios-glyphs/30/000000/shocker-emoji.png"/>
+    <input value={text}
+      placeholder=""
+      onChange={e=> setText(e.target.value)}
+    />
+    <img onClick={()=> {
+      props.onSend(text)
+      setText("")
+    }}
+      src="https://img.icons8.com/windows/32/000000/long-arrow-right.png"
+      id = "send-arrow"
+    />
+  </div>
 }
 
 export default App;
